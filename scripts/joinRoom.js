a9175f04647e04e92948f7c495e74d16623cc431
zaharenkoliza <Liza93087@gmail.com>
2025-01-19 20:54:18 +0300

    feat(join-room): name required

--- scripts/joinRoom.js
+++ scripts/joinRoom.js
@@ -4,26 +4,31 @@ buttons.forEach(button => {
 	button.addEventListener('click', (e) => {
 		e.preventDefault();
 		const idRoom = button.dataset.idRoom;
-		const name = 'lliza';
+		const name = document.querySelector('input[name="name"]').value.trim();
 		const messageDiv = document.getElementById('message');
 
-		fetch('../api/join_room.php', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			body: `room=${encodeURIComponent(idRoom)}&name=${encodeURIComponent(name)}`
-		})
-		.then(response => response.json())
-		.then(data => {
-			messageDiv.textContent = data.message;
-
-			if (data.status === 'success' || data.message === 'You are already in this room') {
-				window.location.href = './game.php?room=' + idRoom;
-			}
-		})
-		.catch(error => {
-			messageDiv.textContent = 'An error occurred during the request';
-		});
+		if (!name) {
+			alert('Введите имя!');
+		}
+		else {
+			fetch('../api/join_room.php', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: `room=${encodeURIComponent(idRoom)}&name=${encodeURIComponent(name)}`
+			})
+			.then(response => response.json())
+			.then(data => {
+				messageDiv.textContent = data.message;
+	
+				if (data.status === 'success' || data.message === 'You are already in this room') {
+					window.location.href = './game.php?room=' + idRoom;
+				}
+			})
+			.catch(error => {
+				messageDiv.textContent = 'An error occurred during the request';
+			});
+		}
 	});
 });
