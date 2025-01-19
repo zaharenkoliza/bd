0c6fd769761160e352469155bb675090d055e684
zaharenkoliza <Liza93087@gmail.com>
2025-01-19 20:53:49 +0300

    new_room

---
+++ scripts/newRoom.js
@@ -0,0 +1,40 @@
+export function initNewRoom() {
+	const form = document.getElementById('create-room-form');
+
+	if (!form) return;
+
+	form.addEventListener('submit', function (e) {
+		e.preventDefault();
+		const amountPlayers = form.querySelector('input[name=amount-players]').value;
+		const timeMove = form.querySelector('input[name=time-move]').value;
+		const messageDiv = document.getElementById('message');
+
+		fetch('../api/new_room.php', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: `amount_of_players=${encodeURIComponent(amountPlayers)}&time_for_move=${encodeURIComponent(timeMove)}`
+		})
+		.then(response => {
+			if (!response.ok) {
+				return Promise.reject('An error occurred during the request');
+			}
+			return response.json();
+		})
+		.then(data => {
+			messageDiv.textContent = data.message;
+			console.log(data.message);
+			if (data.message === 'success') {
+				const idRoom = data['info']['id_room'];
+				console.log(idRoom);
+				alert("Id созданной вами комнаты:" + idRoom);
+				window.location.href = './rooms.php';
+			}
+		})
+		.catch(error => {
+			messageDiv.textContent = error;
+			console.error(error);
+		});
+	});
+}
